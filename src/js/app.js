fd3e516e2c6c99c4de6c240a2ea2c8609efd8b9f
Francis Lagares <francis.lagares@gmail.com>
2020-05-30 19:41:26 +0200

    feat: create the recipe controller

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the application
@@ -12,6 +14,9 @@ import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
 
+/**
+ * Search Controller
+ */
 const controlSearch = async () => {
   // 1. Get query from view
   const query = searchView.getInput();
@@ -23,11 +28,16 @@ const controlSearch = async () => {
     searchView.clearInput();
     searchView.clearResults();
     renderLoader(elements.searchRes);
-    // 4. Search for recipes
-    await state.search.getResults();
-    // 5. Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.results);
+    try {
+      // 4. Search for recipes
+      await state.search.getResults();
+      // 5. Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.results);
+    } catch (err) {
+      console.log('Error processing the search...');
+      clearLoader();
+    }
   }
 };
 
@@ -43,4 +53,53 @@ elements.searchResPages.addEventListener('click', (e) => {
     searchView.clearResults();
     searchView.renderResults(state.search.results, goToPage);
   }
+});
+
+/**
+ * Recipe Controller
+ */
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+  if (id) {
+    // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+    // Highlight selected search item
+    if (state.search) searchView.highlightSelected(id);
+    try {
+      // Create new recipe object
+      state.recipe = new Recipe(id);
+      // Get recipe data
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+      // Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (err) {
+      alert('Error processing recipe!');
+    }
+  }
+};
+
+['hashchange', 'load'].forEach((event) => window.addEventListener(event, controlRecipe));
+
+// Handling recipe buttons clicks
+elements.recipe.addEventListener('click', (e) => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    // Increase button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+  console.log(state.recipe);
 });

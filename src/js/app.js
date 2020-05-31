78626e7e8650a6689568f7e22182581eb845041a
Francis Lagares <francis.lagares@gmail.com>
2020-05-31 12:54:18 +0200

    feat: create list controller

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the application
@@ -13,6 +15,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
  **/
 
 const state = {};
+window.state = state;
 
 /**
  * Search Controller
@@ -61,7 +64,6 @@ elements.searchResPages.addEventListener('click', (e) => {
 const controlRecipe = async () => {
   // Get ID from url
   const id = window.location.hash.replace('#', '');
-  console.log(id);
   if (id) {
     // Prepare UI for changes
     recipeView.clearRecipe();
@@ -88,6 +90,62 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach((event) => window.addEventListener(event, controlRecipe));
 
+/**
+ * List Controller
+ */
+const controlList = () => {
+  // Create a new list IF there is none yet
+  if (!state.list) {
+    state.list = new List();
+
+    // Add each ingredient to the list
+    state.recipe.ingredients.forEach((el) => {
+      const item = state.list.addItem(el.count, el.unit, el.ingredient);
+      listView.renderItem(item);
+    });
+  } else {
+    state.recipe.ingredients.forEach((el) => {
+      // Check for duplicates
+      const id = state.list.duplicateID(el.unit, el.ingredient);
+      if (!id) {
+        // Add ingredients to list if not duplicate
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+      } else {
+        // Update count if duplicate
+        const count = state.list.getCount(id) + el.count;
+        listView.updateItem(id, count);
+        state.list.updateCount(id, count);
+      }
+    });
+  }
+};
+
+// Handle delete and update list events
+elements.shopping.addEventListener('click', (e) => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+  // Handle delete button
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // Delete item from state
+    state.list.deleteItem(id);
+    // Delete from UI
+    listView.deleteItem(id);
+    // Handle the count update
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
+// Handle the count update
+elements.shopping.addEventListener('change', (e) => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+  if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // Handling recipe buttons clicks
 elements.recipe.addEventListener('click', (e) => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -100,6 +158,10 @@ elements.recipe.addEventListener('click', (e) => {
     // Increase button is clicked
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    // Add ingredients to shopping list
+    controlList();
   }
-  console.log(state.recipe);
 });
+
+window.l = new List();
